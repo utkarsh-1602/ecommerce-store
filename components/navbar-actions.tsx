9c10151920ec075249e92574fa2cdc9f4de824fa
utkarsh-1602 <usking619@gmail.com>
2023-11-22 15:04:05 +0530

    added : custom Button component, shoppingBag icon

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,9 +1,20 @@
 "use client"
 
+import Button from "@/components/ui/button";
+import { ShoppingBag } from "lucide-react";
+
 const NavbarActions = () => {
     return (
         <div className="ml-auto flex items-center gap-x-4">
-            Hello Actions
+            <Button className=" flex items-center px-4 py-2">
+                <ShoppingBag
+                    size={20}
+                    color="white"
+                />
+                <span className="ml-2 text-sm font-medium text-white">
+                    0
+                </span>
+            </Button>
         </div>
     )
 }
---
+++ components/ui/button.tsx
@@ -0,0 +1,58 @@
+import { cn } from "@/lib/utils";
+import { forwardRef } from "react";
+
+export interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> { }
+
+// ButtonProps interface is inheriting ButtonHTMLAttributes than is being provided by HTML. 
+// all the properties which ButtonHTMLAttributes have, can be used by ButtonProps as well. 
+
+// HTMLButtonElement is an interface provided by DOM that defines the properties and methods available for HTML button elements. It includes attributes like disabled, form, type, and methods like click().
+
+// React.ButtonHTMLAttributes is another interface provided by React. It includes all the standard HTML attributes that can be applied to a button element, such as onClick, className, disabled, etc. 
+
+// ButtonHTMLAttributes is a generic type that takes HTMLButtonElement as its type parameter
+
+
+// This part is using TypeScript generics
+// This is using the forwardRef function from React to forward the ref to the underlying button element. The first generic parameter (HTMLButtonElement) specifies the type of the ref that will be forwarded, and the second parameter (ButtonProps) specifies the type of props that this component will receive.
+
+// ...props: This syntax is the spread operator in JavaScript/TypeScript
+// ...props is used to pass any additional props that your Button component might receive
+// for example, <Button customProp="someValue" onClick={handleClick}> Click me </Button>
+// The customProp and onClick props would be collected by ...props, and they will be spread onto the underlying <button> element 
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    className,
+    children,
+    disabled,
+    type = "button",
+    ...props
+}, ref) => {
+    return (
+        <button
+            className={cn(`
+                w-auto
+                rounded-full 
+                bg-black 
+                border-transparent 
+                px-5 py-3 
+                disabled:cursor-not-allowed 
+                disabled:opacity-50 
+                text-white 
+                font-semibold 
+                hover:opacity-75
+                transition
+            `, className)}
+            ref={ref}
+        >
+            {children}
+        </button>
+    )
+});
+
+Button.displayName = "Button"
+
+
+// it is our owen custom button component 
+export default Button
