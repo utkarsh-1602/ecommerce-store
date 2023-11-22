4af5b354fdc85c79d6b8eca6d6c8142499977bcb
utkarsh-1602 <usking619@gmail.com>
2023-11-22 21:33:14 +0530

    fetched Products list

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -1,10 +1,29 @@
+import qs from 'query-string'
 import { Product } from "@/types";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`
 
-const getCategories = async (): Promise<Product[]> => {
-    const res = await fetch(URL)
+interface Query {
+    categoryId?: string;
+    colorId?: string;
+    sizeId?: string;
+    isFeatured?: boolean;
+}
+
+const getProducts = async (query: Query): Promise<Product[]> => {
+
+    const url = qs.stringifyUrl({
+        url: URL,
+        query: {
+            colorId: query.colorId,
+            sizeId: query.sizeId,
+            categoryId: query.categoryId,
+            isFeatured: query.isFeatured
+        }
+    });
+
+    const res = await fetch(url)
     return res.json()
 }
 
-export default getCategories;
+export default getProducts;
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,16 +1,22 @@
 
 import getBillboards from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard"
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container"
 
 const HomePage = async () => {
 
+    const products = await getProducts({ isFeatured: true });
     const billboard = await getBillboards("defc42b7-013c-4cff-a1a1-4eabffbe084b")
 
     return (
         <Container>
-            <div className="space-y-10 pb-10 text-white shadow-md" >
+            <div className="space-y-10 pb-10 text-white" >
                 <Billboard data={billboard} />
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 ">
+                <ProductList title="Featured Products" items={products} />
             </div>
         </Container>
     )
